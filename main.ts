75b3a8b1ebe6be959cf7bbfc0dff8835259020df
Admin <info@codefromanywhere.com>
2024-12-29 19:20:34 +0100

    improvements

--- main.ts
+++ main.ts
@@ -30,39 +30,40 @@ curl -X POST \
 
 */
 
-Deno.serve(async (request: Request) => {
-  try {
-    const {
-      code,
-      item,
-      index,
-      array,
-    }: { code: string; item: any; index: number; array: any[] } =
-      await request.json();
+export default {
+  fetch: async (request: Request) => {
+    try {
+      const {
+        code,
+        item,
+        index,
+        array,
+      }: { code: string; item: any; index: number; array: any[] } =
+        await request.json();
 
-    if (
-      code === undefined ||
-      item === undefined ||
-      index === undefined ||
-      array === undefined
-    ) {
-      return new Response(
-        JSON.stringify({
-          status: 400,
-          result: undefined,
-        }),
-        { status: 400 },
-      );
-    }
+      if (
+        code === undefined ||
+        item === undefined ||
+        index === undefined ||
+        array === undefined
+      ) {
+        return new Response(
+          JSON.stringify({
+            status: 400,
+            result: undefined,
+          }),
+          { status: 400 },
+        );
+      }
 
-    const fullCode = `(async () => {
+      const fullCode = `(async () => {
   try {
     const fn = ${code};
     return {
       status: 200,
       result: await fn(${JSON.stringify(item)}, ${index}, ${JSON.stringify(
-      array,
-    )})
+        array,
+      )})
     };
   } catch (err) {
     return {
@@ -73,20 +74,22 @@ Deno.serve(async (request: Request) => {
   }
 })()`;
 
-    const evaluated = await eval(fullCode);
-    console.log({ fullCode, evaluated });
+      const evaluated = await eval(fullCode);
+      console.log({ fullCode, evaluated });
 
-    return new Response(JSON.stringify(evaluated), {
-      status: evaluated.status,
-    });
-  } catch (e: any) {
-    console.error(e);
-    return new Response(
-      JSON.stringify({
-        status: e.cause || 500,
-        result: undefined,
-      }),
-      { status: e.cause || 500 },
-    );
-  }
-});
+      return new Response(JSON.stringify(evaluated), {
+        status: evaluated.status,
+      });
+    } catch (e: any) {
+      console.error(e);
+      return new Response(
+        JSON.stringify({
+          status: e.cause || 500,
+          error: e.message,
+          result: undefined,
+        }),
+        { status: e.cause || 500 },
+      );
+    }
+  },
+};
